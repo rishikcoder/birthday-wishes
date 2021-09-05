e8b74b004c75024772c6e263fb1e2655dcea881d
rishikcoder <84475398+rishikcoder@users.noreply.github.com>
2021-09-05 11:03:29 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+var canvas = new fabric.Canvas('myCanvas');
+ var x= document.getElementById("myAudio");
+
+function new_image()
+{
+   
+    
+
+    
+	
+}
+
+function playSound(){
+x.playSound();
+}
+canvas.add(block_image_object);
+block_image_object.set({
+  top:0,
+  left:0  
+});
